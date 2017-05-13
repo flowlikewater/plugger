cb8b97ad14d64741fe598902f012d93e9402f6e7
Kevin <cho.kevin@hotmail.com>
2017-05-13 19:04:20 +0800

    add authorization to post creation

--- spec/features/user_creates_post_spec.rb
+++ spec/features/user_creates_post_spec.rb
@@ -1,20 +1,36 @@
 require 'rails_helper'
 
 RSpec.feature "Creating post" do
+  let(:user) { User.create(username: "exampleuser", email: "example@user.com", password: "password", password_confirmation: "password")}
+
   scenario "succesfully" do
+    sign_in user
     visit root_path
     click_on "Write a story"
     fill_in "Title", with: "My first post"
     fill_in "Body", with: "Some awesome content"
     click_on "Publish"
-    expect(page).to have_content "My first post"
+    within(".posts") do
+      expect(page).to have_content "My first post"
+      expect(page).to have_content "exampleuser"
+    end
   end
+
   scenario "unsuccessfully" do
+    sign_in user
     visit root_path
     click_on "Write a story"
     fill_in "Title", with: "My second post"
     fill_in "Body", with: ""
     click_on "Publish"
+
     expect(page).to have_css ".error"
+  end
+
+  scenario "non-logged in user cannot create post" do
+    visit root_path
+    click_on "Write a story"
+
+    expect(current_path).to eq(new_user_session_path)
   end
 end
