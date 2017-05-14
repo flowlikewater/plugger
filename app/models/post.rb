6b967e49e308330b45860033989f303d7a1a4d46
Kevin <cho.kevin@hotmail.com>
2017-05-14 19:20:08 +0800

    allow users to add tags to post, search by tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,24 @@
 class Post < ApplicationRecord
   validates :title, :body, presence: true
 
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
+
   belongs_to :user
+
+  # create method to create association between post and tags, e.g. post.all_tags('Music', "Travel")
+  def all_tags=(names)
+    self.tags = names.split(',').map do |name|
+      Tag.where(name: name).first_or_create!
+    end
+  end
+
+  def all_tags
+    tags.map(&:name).join(", ")
+  end
+
+  def self.tagged_with(name)
+    Tag.find_by!(name: name).posts
+  end
+
 end
